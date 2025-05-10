ef7c66f128e82e77bc6bff56bc7985aeb9870be8
Yismen Jorge <yismen.jorge@gmail.com>
2025-05-10 13:44:49 -0400

    Make 'address' field in clients table non-nullable

--- database/migrations/2025_05_01_193656_create_clients_table.php
+++ database/migrations/2025_05_01_193656_create_clients_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->text('address')->nullable();
+            $table->text('address');
             $table->softDeletes();
             $table->timestamps();
         });
