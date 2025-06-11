3e7030dfa1af34ac5084a9152c5e7d36e30e4ef3
Yismen Jorge <yismen.jorge@gmail.com>
2025-06-11 08:00:06 -0400

    Enhance UserResource table: add searchable roles column with badge and separator, and update visibility for edit and delete actions

--- app/Filament/Admin/Resources/UserResource.php
+++ app/Filament/Admin/Resources/UserResource.php
@@ -49,6 +49,12 @@ class UserResource extends Resource
                 Tables\Columns\TextColumn::make('email_verified_at')
                     ->dateTime()
                     ->sortable(),
+                Tables\Columns\TextColumn::make('roles.name')
+                    ->label('Roles')
+                    ->searchable()
+                    ->badge()
+                    ->separator(',')
+                    ->sortable(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
@@ -62,8 +68,10 @@ class UserResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\EditAction::make()
+                    ->visible(fn ($record) => $record->id !== auth()->id() && ! $record->hasRole('super-admin')),
+                Tables\Actions\DeleteAction::make()
+                    ->visible(fn ($record) => $record->id !== auth()->id() && ! $record->hasRole('super-admin')),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
