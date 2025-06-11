c399d98ec2380437b39087f0e2c51b1b47c5e739
Yismen Jorge <yismen.jorge@gmail.com>
2025-06-11 12:21:02 -0400

    Remove uniqueness constraint from item name field in migration

--- database/migrations/2025_05_02_200457_create_items_table.php
+++ database/migrations/2025_05_02_200457_create_items_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->foreignIdFor(Campaign::class)->constrained()->cascadeOnDelete();
             $table->decimal('price', 10, 2);
             $table->string('description')->nullable();
