8598ce98b4b5ae75de05efdc97ee3b4aaedf0313
Yismen Jorge <yismen.jorge@gmail.com>
2025-08-13 14:09:27 -0400

    Adds custom validation for unique names in forms
    Implements custom validation rules for the 'name' field in both AgentForm and CampaignForm to ensure uniqueness based on project and agent context.
    
    This change enhances data integrity by preventing duplicate entries for names associated with specific projects and agents, improving user experience and form reliability.

--- app/Services/Filament/Forms/AgentForm.php
+++ app/Services/Filament/Forms/AgentForm.php
@@ -2,7 +2,10 @@
 
 namespace App\Services\Filament\Forms;
 
+use Closure;
 use Filament\Forms;
+use App\Models\Agent;
+use Filament\Forms\Get;
 
 class AgentForm
 {
@@ -11,7 +14,25 @@ class AgentForm
         return [
             Forms\Components\TextInput::make('name')
                 ->required()
-                ->unique(ignoreRecord: true)
+                ->rules([
+                    'string',
+                    'max:255',
+                    function(Get $get, $livewire, $record): Closure {
+                        return function (string $attribute, $value, Closure $fail) use($get, $livewire, $record) {
+                            $record_exists = Agent::query()
+                                ->where('name', $value)
+                                ->where('project_id', $livewire->data['project_id'])
+                                ->when($record, function ($query) use ($record) {
+                                    return $query->where('id', '!=', $record->id);
+                                })
+                                ->exists();
+
+                            if ($record_exists) {
+                                $fail("The name {$value} already exits for this project.");
+                            }
+                        };
+                    },
+                ])
                 ->autofocus()
                 ->maxLength(255),
             Forms\Components\Select::make('project_id')
--- app/Services/Filament/Forms/CampaignForm.php
+++ app/Services/Filament/Forms/CampaignForm.php
@@ -2,7 +2,10 @@
 
 namespace App\Services\Filament\Forms;
 
+use Closure;
 use Filament\Forms;
+use Filament\Forms\Get;
+use App\Models\Campaign;
 
 class CampaignForm
 {
@@ -12,8 +15,27 @@ class CampaignForm
             Forms\Components\TextInput::make('name')
                 ->autofocus()
                 ->required()
-                ->unique(ignoreRecord: true)
-                ->maxLength(255),
+                ->rules([
+                    'string',
+                    'max:255',
+                    function(Get $get, $livewire, $record): Closure {
+                        return function (string $attribute, $value, Closure $fail) use($get, $livewire, $record) {
+                            $record_exists = Campaign::query()
+                                ->where('name', $value)
+                                ->where('agent_id', $livewire->data['agent_id'])
+                                ->when($record, function ($query) use ($record) {
+                                    return $query->where('id', '!=', $record->id);
+                                })
+                                ->exists();
+
+                            if ($record_exists) {
+                                $fail("The name {$value} already exits for this agent.");
+                            }
+                        };
+                    },
+                ])
+                ->maxLength(255)
+                ,
             Forms\Components\Select::make('agent_id')
                 ->relationship('agent', 'name')
                 ->createOptionForm(AgentForm::make())
