a295e78c600c6e2076ab5caad2821adbb3522695
Yismen Jorge <yismen.jorge@gmail.com>
2025-08-28 08:54:42 -0400

    Removes commented-out render hooks in FilamentServiceProvider and updates vite directive in conversations view

--- app/Providers/FilamentServiceProvider.php
+++ app/Providers/FilamentServiceProvider.php
@@ -26,16 +26,16 @@ class FilamentServiceProvider extends ServiceProvider
             $panelSwitch->simple();
         });
 
-        // Add custom CSS
-        Filament::registerRenderHook(
-            'panels::head.end',
-            fn (): string => '<link rel="stylesheet" href="' . Vite::asset('resources/css/app.css') . '">'
-        );
+        // // Add custom CSS
+        // Filament::registerRenderHook(
+        //     'panels::head.end',
+        //     fn (): string => '<link rel="stylesheet" href="' . Vite::asset('resources/css/app.css') . '">'
+        // );
 
-        // Add custom JS
-        Filament::registerRenderHook(
-            'panels::body.end',
-            fn (): string => '<script type="module" src="' . Vite::asset('resources/js/app.js') . '"></script>'
-        );
+        // // Add custom JS
+        // Filament::registerRenderHook(
+        //     'panels::body.end',
+        //     fn (): string => '<script type="module" src="' . Vite::asset('resources/js/app.js') . '"></script>'
+        // );
     }
 }
--- resources/views/filament/chat/pages/conversations.blade.php
+++ resources/views/filament/chat/pages/conversations.blade.php
@@ -1,6 +1,6 @@
 <x-filament-panels::page>
     @section('title', __('filament-chat::chat.pages.conversations.title'))
-
+    @vite([ 'resources/js/app.js', 'resources/css/app.css' ])
     <div class="flex justify-between gap-6"  style="height: calc(100vh - 12rem);">
         {{-- Conversations --}}
         {{-- Users --}}
