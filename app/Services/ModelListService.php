2278ce30481c68020d9e571d5ef54974949991b0
Yismen Jorge <yismen.jorge@gmail.com>
2025-10-24 18:41:23 -0400

    Enhances ModelListService with improved caching and conditions
    Improves the ModelListService by refactoring the caching mechanism for model retrieval and enhancing the handling of conditional queries.
    
    Introduces a new private method for better organization and clarity.
    
    This change addresses performance by optimizing the cache usage and allows for more flexible query conditions, particularly supporting 'whereIn' conditions.

--- app/Services/ModelListService.php
+++ app/Services/ModelListService.php
@@ -5,6 +5,16 @@ namespace App\Services;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Cache;
 
+/**
+ * Service for retrieving lists of models with optional conditions and caching.
+ *
+ * Usage:
+ * - Call ModelListService::get() with a model class or query builder, key and value fields, and optional conditions.
+ * - Returns an associative array of key-value pairs from the model, cached for performance.
+ *
+ * Example:
+ *   ModelListService::get(User::class, 'id', 'name', [['active', '=', 1]]);
+ */
 class ModelListService
 {
     public static function get(
@@ -19,22 +29,27 @@ class ModelListService
 
         $model = $model instanceof Builder ? $model : $model::query();
 
-        return Cache::rememberForever(self::getCacheKey($model, $conditions), function () use (
-            $model,
-            $key_field,
-            $value_field,
-            $conditions,
-        ) {
-            $model = $model
-                ->orderBy($value_field);
+        return Cache::rememberForever(
+            self::getCacheKey($model, $conditions),
+            fn() => self::getResults($model, $key_field, $value_field, $conditions)
+        );
+    }
 
-            foreach ($conditions as $condition) {
+    private static function getResults( $model, $key_field, $value_field, $conditions) {
+        $model = $model
+            ->orderBy($value_field);
+
+        foreach ($conditions as $key => $condition) {
+            \Illuminate\Support\Facades\Log::info($key == 'in');
+            if ($key == 'in') {
+                $model->whereIn($condition[0], $condition[1]);
+            } else {
                 $model->where($condition);
             }
+        }
 
-            return $model->pluck($value_field, $key_field)
-                ->toArray();
-        });
+        return $model->pluck($value_field, $key_field)
+            ->toArray();
     }
 
     private static function getCacheKey($model, array $condition): string
