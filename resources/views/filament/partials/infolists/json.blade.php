268e9f9de627a4ef642ea65bfbd79769dbad4cbd
Yismen Jorge <yismen.jorge@gmail.com>
2025-11-03 10:07:16 -0400

    Refactor JSON infolist rendering to use list elements for better structure and readability

--- resources/views/filament/partials/infolists/json.blade.php
+++ resources/views/filament/partials/infolists/json.blade.php
@@ -1,7 +1,13 @@
 @foreach ($json as $key => $item)
-    <h5>{{ str($key)->headline() }}</h5>
-    @foreach ($item as $elementKey => $elementValue)
-        <p style="margin-left: 20px;">{{ $elementKey }}: {{ is_array($elementValue) ? \collect($elementValue)->toPrettyJson() : $elementValue }}</p>
-    @endforeach
+    <h5>
+        <strong>{{ str($key)->headline() }}</strong>
+    </h5>
+    <ul style="margin-left: 20px;">
+        @foreach ($item as $subKey => $subItem)
+            <li>
+                <strong>{{ str($subKey)->headline() }}:</strong> {{ is_array($subItem) || is_object($subItem) ? json_encode($subItem) : $subItem }}
+            </li>
+        @endforeach
+    </ul>
     <hr>
 @endforeach
